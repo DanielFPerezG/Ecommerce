4afa018e4fee6a896a9004995f32159b3f60bb1d
Daniel Pérez <danielperezgalindo@gmail.com>
2022-12-30 17:00:03 -0500

    Login and Home User Pages

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from django.contrib import admin
+
 from . import views
 
 app_name = "base"   
 
 
 urlpatterns = [
-    path('', views.loginPage, name="login")
+    path('admin/', admin.site.urls),
+    path('', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('home', views.home, name="home")
 ]
--- base/views.py
+++ base/views.py
@@ -1,31 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import User
 
 # Create your views here.
 
 def loginPage(request):
     page = 'login'
-    # if request.user.is_authenticated:
-    #     return redirect('home')
+    if request.user.is_authenticated:
+        return redirect('home')
 
-    # if request.method == 'POST':
-    #     email = request.POST.get('email').lower()
-    #     password = request.POST.get('password')
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
 
-    #     try:
-    #         user = User.objects.get(email = email)
-    #     except:
-    #         messages.error(request, 'User does not exist')
+        try:
+            user = User.objects.get(email = email)
+        except:
+            messages.error(request, 'User does not exist')
         
-    #     user = authenticate(request, email = email, password=password)
+        user = authenticate(request, email = email, password=password)
 
-    #     if user is not None:
-    #         login(request, user)
-    #         return redirect('home')
-    #     else:
-    #         messages.error(request, 'Username or password is incorrect')
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password is incorrect')
 
     context= {'page': page}
     return render(request, 'base/login_register.html', context)
 
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+@login_required(login_url='login')
 def home(request):
     return render(request, 'base/home.html')
