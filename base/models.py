b5b455b1a0a67fb86361df1b4767302d4a416d30
Daniel Pérez <danielperezgalindo@gmail.com>
2022-12-30 17:31:54 -0500

    Create product model

--- base/models.py
+++ base/models.py
@@ -7,3 +7,19 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+class Product(models.Model):
+    name = models.CharField(max_length=150,null=True)
+    bio = models.CharField(max_length=1000,null=True)
+    image = models.ImageField(null=True)
+    price = models.PositiveIntegerField(null=True)
+    stock = models.PositiveIntegerField(null=True)
+    topic = models.CharField(max_length=150, null=True)
+    cost = models.PositiveIntegerField(null=True)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-updated','-created']
+    
