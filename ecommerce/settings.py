a4780d18b13d71fd6dc1dfefc0cffd89cbcd81e3
Daniel Pérez <danielperezgalindo@gmail.com>
2023-01-03 11:39:02 -0500

    Change DB from SQLite to MYSQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -93,8 +93,14 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': 3306
+
+
     }
 }
 
