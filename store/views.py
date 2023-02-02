16d86ca1bd657f3f67a76b5e3a00123a7c049e9e
Daniel Pérez <danielperezgalindo@gmail.com>
2023-02-02 15:45:09 -0500

    Navbar add topics from database

--- store/views.py
+++ store/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 
-from base.models import Product
+from base.models import Product, Topic
 
 # Create your views here.
 
 
 def home(request):
     products = Product.objects.all()
-    context = {'products':products}
+    topics = Topic.objects.all()
+    context = {'products':products,'topics':topics}
     return render(request, 'store/home.html', context)
