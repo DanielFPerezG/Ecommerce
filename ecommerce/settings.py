b44b4c8c059fbaa8aa473245f53240a571cd404e
Daniel Pérez <danielperezgalindo@gmail.com>
2023-07-22 00:55:46 -0500

    Update Statics File

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -155,17 +155,13 @@ STATIC_URL = '/static/'
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
