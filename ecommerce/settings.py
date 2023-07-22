c27c72117d51d73933a01a5d9642ee4b57665cc2
Daniel Pérez <danielperezgalindo@gmail.com>
2023-07-22 01:09:25 -0500

    Add Loggin Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -171,3 +171,26 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',  # You can set this to 'DEBUG' for more detailed logs
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',  # You can set this to 'DEBUG' for more detailed logs
+            'propagate': False,
+        },
+    },
+}
