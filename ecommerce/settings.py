ccd4ec9fc3ff895c33e079fd836276cc09fe5d8f
Daniel Pérez <danielperezgalindo@gmail.com>
2023-07-23 21:22:43 -0500

    Update Subdomain Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,9 +31,7 @@ ROOT_HOSTCONF = 'ecommerce.hosts'
 
 DEFAULT_HOST = ' '
 
-ALLOWED_HOSTS = [
-    "*"
-]
+ALLOWED_HOSTS = ['.danielperez.engineer', 'base.danielperez.engineer']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
