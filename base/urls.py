6b9fc018b18b5b16c12453f4bee360db213f8372
Daniel Pérez <danielperezgalindo@gmail.com>
2023-07-25 22:06:07 -0500

    Test re_path

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.urls import re_path
+
 from . import views
 
 app_name = "base"
@@ -28,4 +31,8 @@ urlpatterns = [
     path('deleteBanner/<str:pk>', views.deleteBanner, name="deleteBanner"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,10 +20,19 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django_hosts import patterns, host
 
+from django.views.static import serve
+from django.urls import re_path
+
 # URL patterns regulares de Django
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
