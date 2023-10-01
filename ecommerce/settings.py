cc775983a5f2b50e54dc52faf34af1ebca28e2d2
Daniel Pérez <danielperezgalindo@gmail.com>
2023-09-30 21:29:30 -0500

    Update Data Base Settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommerce_db',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': 3306
 
