d2fd4458ceb2c30abc70dfa5c18a4ff9389b8502
Daniel Pérez <danielperezgalindo@gmail.com>
2023-10-10 17:05:26 -0500

    Update wsgi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -13,6 +13,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ecommerce.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
 application = WSGIHandler()
