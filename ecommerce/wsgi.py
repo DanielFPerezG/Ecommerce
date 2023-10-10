1308c34485006ce52d3c8e8fcbfd5390144c5b32
Daniel Pérez <danielperezgalindo@gmail.com>
2023-10-10 17:14:20 -0500

    Update wsgi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -6,13 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-from django.contrib.auth.handlers.modwsgi import check_password, groups_for_user
-from django.core.handlers.wsgi import WSGIHandler
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
-application = WSGIHandler()
+application = get_wsgi_application()
