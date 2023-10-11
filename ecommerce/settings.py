cfd34c199e737a5ff3dce1133aedb2ed736f38d8
Daniel Pérez <danielperezgalindo@gmail.com>
2023-10-11 15:13:30 -0500

    Update Media Path

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -173,7 +173,7 @@ MEDIA_URL = '/images/'
 #STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, "images/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
