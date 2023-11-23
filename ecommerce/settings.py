0854ac6da76327a67d7afd9f1dad4ae1fd8a52ac
Daniel Pérez <danielperezgalindo@gmail.com>
2023-11-23 16:21:14 -0500

    Refactor Media Folder

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -165,13 +165,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
